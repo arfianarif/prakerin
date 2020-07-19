d7e69564fd2f769be2c30f441b2699620778b413
arfianarif <arfianarif11@gmail.com>
2020-07-19 19:06:10 +0700

    example modal edit ajax

--- application/controllers/Admin/Master_Data/Siswa.php
+++ application/controllers/Admin/Master_Data/Siswa.php
@@ -3,37 +3,54 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Siswa extends CI_Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
+	public function __construct()
+	{
+		parent::__construct();
 
-        $this->load->library('session');
-        // $this->load->model('Auth/MLogin');
-    }
-    public function index()
-    {
-        $this->data['content'] = $this->db->get('m_siswa')->result();
-        // $this->data['custom_js'] = $this->load->view('Admin/js/admin');
-        $this->template->load('Admin/Master_Data/Siswa/index', $this->data);
-    }
-    public function add()
-    {
-        $data = array(
-            'email' => $this->input->post('email'),
-            'password' => $this->input->post('password'),
-            'id_role' => 4
-        );
-        $this->db->insert('m_siswa', $data);
-        redirect('Admin/Master_Data/Siswa');
-    }
-    public function delete($id)
-    {
-        $this->db->delete('m_siswa', array('id' => $id));
-        redirect('Admin/Master_Data/Siswa');
-    }
-    public function getData($id)
-    {
-        $output = $this->db->get_where('m_siswa', ['id' => $id])->result();
-        $this->output->set_content_type('application/json')->set_output(json_encode($output));
-    }
+		$this->load->library('session');
+		// $this->load->model('Auth/MLogin');
+	}
+	public function index()
+	{
+		$this->data['content'] = $this->db->get('m_siswa')->result();
+		// $this->data['custom_js'] = $this->load->view('Admin/js/admin');
+		$this->template->load('Admin/Master_Data/Siswa/index', $this->data);
+	}
+	public function add()
+	{
+		$data = array(
+			'email' => $this->input->post('email'),
+			'password' => $this->input->post('password'),
+			'id_role' => 4
+		);
+		$this->db->insert('m_siswa', $data);
+		redirect('Admin/Master_Data/Siswa');
+	}
+	public function delete($id)
+	{
+		$this->db->delete('m_siswa', array('id' => $id));
+		redirect('Admin/Master_Data/Siswa');
+	}
+	public function getData($id)
+	{
+		$output = $this->db->get_where('m_siswa', ['id' => $id])->row();
+		$this->output->set_content_type('application/json')->set_output(json_encode($output));
+	}
+	public function update()
+	{
+		// echo '<pre>';
+		// print_r($this->input->post());
+		// exit;
+		$data = array(
+			'id' => $this->input->post('id'),
+			'email'  => $this->input->post('email'),
+			'password'  => $this->input->post('password'),
+			'nama_siswa'  => $this->input->post('nama_siswa'),
+			'ttl'  => $this->input->post('ttl'),
+			'alamat'  => $this->input->post('alamat'),
+		);
+		$status = $this->db->replace('m_siswa', $data);
+		// 	echo json_encode($status);
+		redirect('Admin/Master_Data/Siswa');
+	}
 }
--- application/views/Admin/Master_Data/Siswa/index.php
+++ application/views/Admin/Master_Data/Siswa/index.php
@@ -1,214 +1,275 @@
 <div class="card shadow mb-4 border-left-primary">
-    <div class="card-header ">
-        <div class="float-left">
-            <h6 class="font-weight-bold text-primary">Data Master Siswa</h6>
-        </div>
-        <div class="float-right">
-            <a href="#" class="btn btn-sm btn-primary btn-icon-split" data-toggle="modal" data-target="#add">
-                <span class="icon text-white-50">
-                    <i class="fas fa-plus"></i>
-                </span>
-                <span class="text">Tambah Data</span>
-            </a>
-        </div>
-    </div>
-    <div class="card-body">
-        <div class="table-responsive">
-            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                <thead>
-                    <tr>
-                        <th width="5%" align="center">#</th>
-                        <th>Email</th>
-                        <th>Password</th>
-                        <th width="12%">Action</th>
-                    </tr>
-                </thead>
+	<div class="card-header ">
+		<div class="float-left">
+			<h6 class="font-weight-bold text-primary">Data Master Siswa</h6>
+		</div>
+		<div class="float-right">
+			<a href="#" class="btn btn-sm btn-primary btn-icon-split" data-toggle="modal" data-target="#add">
+				<span class="icon text-white-50">
+					<i class="fas fa-plus"></i>
+				</span>
+				<span class="text">Tambah Data</span>
+			</a>
+		</div>
+	</div>
+	<div class="card-body">
+		<div class="table-responsive">
+			<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+				<thead>
+					<tr>
+						<th width="5%" align="center">#</th>
+						<th>Email</th>
+						<th>Password</th>
+						<th width="12%">Action</th>
+					</tr>
+				</thead>
 
-                <tbody>
-                    <?php $i = 1;
-                    foreach ($content as $key) : ?>
-                        <tr>
-                            <?= $key->id ?>
-                            <td align="center"><?= $i++ ?></td>
-                            <td><?= $key->email ?></td>
-                            <td><?= $key->password ?></td>
-                            <td align="center">
-                                <a href="#" data-id="<?= $key->id ?>" class="detail-btn btn btn-info btn-circle btn-sm" data-toggle="modal" data-target="#detail">
-                                    <i class="fas fa-info-circle"></i>
-                                </a>
-                                <button class="edit-btn btn btn-warning btn-circle btn-sm" data-toggle="modal" data-target="#edit" data-id="<?= $key->id ?>">
-                                    <i class="fas fa-exclamation-triangle"></i>
-                                </button>
-                                <a href="<?= base_url() . 'Admin/Master_Data/Siswa/Delete/' . $key->id ?>" class="btn btn-danger btn-circle btn-sm">
-                                    <i class="fas fa-trash"></i>
-                                </a>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
+				<tbody>
+					<?php $i = 1;
+					foreach ($content as $key) : ?>
+						<tr>
+							<?= $key->id ?>
+							<td align="center"><?= $i++ ?></td>
+							<td><?= $key->email ?></td>
+							<td><?= $key->password ?></td>
+							<td align="center">
+								<!-- <a href="#" data-id="<?php //echo $key->id 
+																					?>" class="detail-btn btn btn-info btn-circle btn-sm" data-toggle="modal" data-target="#detail">
+									<i class="fas fa-info-circle"></i>
+								</a> -->
+								<button class="edit-btn btn btn-warning btn-circle btn-sm" data-toggle="modal" data-target="#edit" data-id="<?= $key->id ?>">
+									<i class="fas fa-exclamation-triangle"></i>
+								</button>
+								<a href="<?= base_url() . 'Admin/Master_Data/Siswa/Delete/' . $key->id ?>" class="btn btn-danger btn-circle btn-sm">
+									<i class="fas fa-trash"></i>
+								</a>
+							</td>
+						</tr>
+					<?php endforeach; ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
 </div>
 
 
 <div class="modal fade" id="add" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-scrollable" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Tambah Data</h5>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <?= form_open('Admin/Master_Data/Siswa/add'); ?>
-            <div class="modal-body">
-                <div class="bd-example">
+	<div class="modal-dialog modal-dialog-scrollable" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Tambah Data</h5>
+				<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">×</span>
+				</button>
+			</div>
+			<?= form_open('Admin/Master_Data/Siswa/add'); ?>
+			<div class="modal-body">
+				<div class="bd-example">
 
-                    <div class="form-group">
-                        <label for="email">Email address</label>
-                        <input type="text" class="form-control" id="email-add" name="email" placeholder="name@example.com">
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="text" class="form-control" id="password-add" name="password" placeholder="Password">
-                    </div>
+					<div class="form-group">
+						<label for="email">Email address</label>
+						<input type="text" class="form-control" id="email-add" name="email" placeholder="name@example.com">
+					</div>
+					<div class="form-group">
+						<label for="password">Password</label>
+						<input type="text" class="form-control" id="password-add" name="password" placeholder="Password">
+					</div>
 
-                    <!-- <div class="form-group">
+					<!-- <div class="form-group">
                         <label for="exampleFormControlTextarea1">Example textarea</label>
                         <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                     </div> -->
 
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-dismiss="modal">Close</button>
-                <button class="btn btn-primary" type="submit">Tambah</button>
-            </div>
-            <?= form_close(); ?>
-        </div>
-    </div>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button class="btn btn-secondary" type="button" data-dismiss="modal">Close</button>
+				<button class="btn btn-primary" type="submit">Tambah</button>
+			</div>
+			<?= form_close(); ?>
+		</div>
+	</div>
 </div>
 
-<div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-scrollable" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Edit Data</h5>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <?= form_open('Admin/Master_Data/Siswa/edit'); ?>
-            <div class="modal-body">
-                <div class="bd-example">
+<div id="edit" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-scrollable" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalCenteredScrollableTitle">Edit</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">×</span>
+				</button>
+			</div>
+			<?= form_open('Admin/Master_Data/Siswa/update'); ?>
+			<div class="modal-body">
 
-                    <div class="form-group">
-                        <label for="email">Email address</label>
-                        <input type="text" class="form-control" id="email-edit" name="email" placeholder="name@example.com">
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="text" class="form-control" id="password-edit" name="password" placeholder="Password">
-                    </div>
+				<div class="form-group">
+					<input hidden type="text" class="form-control" id="id-edit" name="id" placeholder="name@example.com">
+				</div>
 
-                    <!-- <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Example textarea</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                    </div> -->
+				<div class="row">
+					<div class="col md-6">
+						<div class="form-group">
+							<label for="email">Email address</label>
+							<input type="text" class="form-control" id="email-edit" name="email" placeholder="name@example.com">
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="form-group">
+							<label for="password">Password</label>
+							<input type="text" class="form-control" id="password-edit" name="password" placeholder="Password">
+						</div>
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="nama_siswa">Nama</label>
+					<input type="text" class="form-control" id="nama_siswa_edit" name="nama_siswa" placeholder="Text Here">
+				</div>
+				<div class="form-group">
+					<label for="ttl">Tempat Tanggal Lahir </label> <br>
 
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-dismiss="modal">Dismiss</button>
-                <button class="btn btn-primary" type="submit">Accept</button>
-            </div>
-            <?= form_close(); ?>
-        </div>
-    </div>
+					<input type="text" class="form-control" id="ttl-edit" name="ttl" placeholder="Text Here">
+				</div>
+				<div class="form-group">
+					<label for="alamat">Alamat </label>
+					<textarea name="alamat" class="form-control" id="alamat-edit" rows="2" placeholder="Text Here"></textarea>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button class="btn btn-secondary" type="button" data-dismiss="modal">Dismiss</button>
+				<button class="btn btn-primary confirm-edit" type="submit" data-id="#">Accept</button>
+			</div>
+			<?= form_close(); ?>
+		</div>
+	</div>
 </div>
-
 <div class="modal fade" id="detail" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-scrollable" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">detail Data</h5>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <?= form_open('Admin/Master_Data/Siswa/detail'); ?>
-            <div class="modal-body">
-                <div class="bd-example">
+	<div class="modal-dialog modal-dialog-scrollable" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">detail Data</h5>
+				<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">×</span>
+				</button>
+			</div>
+			<?= form_open('Admin/Master_Data/Siswa/detail'); ?>
+			<div class="modal-body">
+				<div class="bd-example">
 
-                    <div class="form-group">
-                        <label for="email">Email address</label>
-                        <input type="text" class="form-control" id="email-detail" name="email" placeholder="name@example.com">
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="text" class="form-control" id="password-detail" name="password" placeholder="Password">
-                    </div>
+					<div class="form-group">
+						<label for="email">Email address</label>
+						<input type="text" class="form-control" id="email-detail" name="email" placeholder="name@example.com">
+					</div>
+					<div class="form-group">
+						<label for="password">Password</label>
+						<input type="text" class="form-control" id="password-detail" name="password" placeholder="Password">
+					</div>
 
-                    <!-- <div class="form-group">
+					<!-- <div class="form-group">
                         <label for="exampleFormControlTextarea1">Example textarea</label>
                         <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                     </div> -->
 
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-dismiss="modal">Dismiss</button>
-                <button class="btn btn-primary" type="submit">Accept</button>
-            </div>
-            <?= form_close(); ?>
-        </div>
-    </div>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button class="btn btn-secondary" type="button" data-dismiss="modal">Dismiss</button>
+				<button class="btn btn-primary" type="submit">Accept</button>
+			</div>
+			<?= form_close(); ?>
+		</div>
+	</div>
 </div>
 
 <script>
-    $(document).ready(function() {
-        modalEdit();
-        modalDetail();
-    });
+	$(document).ready(function() {
+		modalEdit();
+		modalDetail();
+	});
 
-    function modalEdit() {
-        $('.edit-btn').click(function() {
-            let base_url = "<?php echo base_url() . 'Admin/Master_Data/Siswa/' ?>";
-            let id = $(this).data('id');
-            console.log(id);
+	function modalEdit() {
+		$('.edit-btn').click(function() {
+			let base_url = "<?php echo base_url() . 'Admin/Master_Data/Siswa/' ?>";
+			let id = $(this).data('id');
+			// $('.id-edit').val(id);
+			// $('.confirm-edit').data('id', id);
+			// console.log(id);
+			$.ajax({
+				url: base_url + 'getData/' + id,
+				type: "POST",
+				dataType: "json",
+				success: function(response) {
+					console.log(response);
+					$('#id-edit').val(response.id);
+					$('#email-edit').val(response.email);
+					$('#password-edit').val(response.password);
+					$('#nama_siswa_edit').val(response.nama_siswa);
+					$('#ttl-edit').val(response.ttl);
+					$('#alamat-edit').val(response.alamat);
+					// $('#edit').modal('handleUpdate');
+					$('#edit').modal('show');
+				}
+			});
+		});
+	}
 
-            $.ajax({
-                url: base_url + 'getData/' + id,
-                type: "POST",
+	// function updateData() {
+	// 	$('.confirm-edit').click(function() {
+	// 		let base_url = "<?php echo base_url() . 'Admin/Master_Data/Siswa/' ?>";
+	// 		let email = $('#email-edit').val();
+	// 		let nama = $('#nama-siswa-edit').val();
+	// 		let password = $('#password-edit').val();
+	// 		let ttl = $('#ttl-edit').val();
+	// 		let alamat = $('#alamat-edit').val();
+	// 		let id = $(this).data('id');
+	// 		console.log(email);
+	// 		console.log(nama);
+	// 		console.log(password);
+	// 		console.log(ttl);
+	// 		console.log(alamat);
+	// 		console.log(id);
+	// 		// $.ajax({
+	// 		// 	type: 'POST',
+	// 		// 	url: base_url + 'update/',
+	// 		// 	dataType: "JSON",
+	// 		// 	data: {
+	// 		// 		id: id,
+	// 		// 		email: email,
+	// 		// 		password: password,
+	// 		// 		nama: nama,
+	// 		// 		ttl: ttl,
+	// 		// 		alamat: alamat,
+	// 		// 	},
 
-                dataType: "json",
-                success: function(response) {
-                    console.log(response);
+	// 		// 	success: function(data) {
+	// 		// 		// $('#add').modal('hide');
+	// 		// 		console.log(data);
+	// 		// 		// window.location.reload();
+	// 		// 	},
+	// 		// 	error: function(result) {
+	// 		// 		alert('error' + result);
+	// 		// 	}
+	// 		// });
+	// 	})
+	// }
 
-                    $('#edit').modal('show');
-                }
-            });
-        });
-    }
+	function modalDetail() {
+		$('.detail-btn').click(function() {
+			let base_url = "<?php echo base_url() . 'Admin/Master_Data/Siswa/' ?>";
+			let id = $(this).data('id');
+			console.log(id);
 
-    function modalDetail() {
-        $('.detail-btn').click(function() {
-            let base_url = "<?php echo base_url() . 'Admin/Master_Data/Siswa/' ?>";
-            let id = $(this).data('id');
-            console.log(id);
+			$.ajax({
+				url: base_url + 'getData/' + id,
+				type: "POST",
 
-            $.ajax({
-                url: base_url + 'getData/' + id,
-                type: "POST",
+				dataType: "json",
+				success: function(response) {
+					console.log(response);
 
-                dataType: "json",
-                success: function(response) {
-                    console.log(response); 
-
-                    $('#detail').modal('show');
-                }
-            });
-        });
-    }
+					$('#detail').modal('show');
+				}
+			});
+		});
+	}
 </script>
