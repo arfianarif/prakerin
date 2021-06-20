c44bb703ff7cec94f25987abd8b893ef46a8cfdd
Unknown <arfianarif11@gmail.com>
2021-06-20 19:04:48 +0700

    new sql

--- prakerin.sql
+++ prakerin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 20, 2021 at 11:13 AM
+-- Generation Time: Jun 20, 2021 at 02:04 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 7.4.20
 
@@ -86,7 +86,9 @@ CREATE TABLE `m_siswa` (
 --
 
 INSERT INTO `m_siswa` (`id_siswa`, `nis`, `nama`, `email`, `password`, `alamat`, `ttl`, `publish`) VALUES
-(13, '01', 'dev-siswa', 'dev-siswa@prakerin.com', '123', 'dev-siswa-alamat', 'dev-siswa-ttl', 1);
+(13, '01', 'dev-siswa', 'dev-siswa@prakerin.com', '123', 'dev-siswa-alamat', 'dev-siswa-ttl', 1),
+(14, '02', 'dev-siswa-2', 'dev-siswa-2@prakerin.com', '123', 'dev-siswa-2-alamat', 'dev-siswa-2-ttl', 1),
+(15, '03', 'dev-siswa-3', 'dev-siswa-3@prakerin.com', '123', 'dev-siswa-3-alamat', 'dev-siswa-3-ttl', 1);
 
 -- --------------------------------------------------------
 
@@ -133,7 +135,9 @@ INSERT INTO `m_users` (`id`, `id_user`, `no_identitas`, `email`, `password`, `ro
 (1, '2', '123', 'admin@gmail.com', '123', 'admin'),
 (15, '13', '01', 'dev-siswa@prakerin.com', '123', 'siswa'),
 (16, '4', '02', 'dev-guru@prakerin.com', '123', 'guru'),
-(17, '3', '03', 'dev-karyawan@prakerin.com', '123', 'tata_usaha');
+(17, '3', '03', 'dev-karyawan@prakerin.com', '123', 'tata_usaha'),
+(18, '14', '02', 'dev-siswa-2@prakerin.com', '123', 'siswa'),
+(19, '15', '03', 'dev-siswa-3@prakerin.com', '123', 'siswa');
 
 -- --------------------------------------------------------
 
@@ -155,13 +159,12 @@ CREATE TABLE `pendaftaran` (
 
 CREATE TABLE `praktik` (
   `id_praktik` int(11) NOT NULL,
-  `id_pendaftaran` int(11) NOT NULL,
   `id_siswa` int(9) NOT NULL,
   `is_group` int(1) NOT NULL,
   `members` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`members`)),
   `nama_instansi` text NOT NULL,
   `alamat_instansi` text NOT NULL,
-  `pubish` int(1) NOT NULL
+  `publish` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -230,7 +233,7 @@ ALTER TABLE `m_guru`
 -- AUTO_INCREMENT for table `m_siswa`
 --
 ALTER TABLE `m_siswa`
-  MODIFY `id_siswa` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_siswa` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `m_tata_usaha`
@@ -242,13 +245,13 @@ ALTER TABLE `m_tata_usaha`
 -- AUTO_INCREMENT for table `m_users`
 --
 ALTER TABLE `m_users`
-  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `pendaftaran`
 --
 ALTER TABLE `pendaftaran`
-  MODIFY `id_pendaftaran` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pendaftaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `praktik`
